e067bc4e7775e9ae14733c64087280c4de3dbc9c
Ivan Munoz-Gutierrez <ivan.munoz.gutierrez@gmail.com>
2023-06-03 18:04:57 -0700

    reorganized project into package

--- src/msp/msplotter.py
+++ src/msp/msplotter.py
@@ -58,7 +58,7 @@ from Bio.SeqRecord import SeqRecord
 from arrows import Arrow
 from user_input import user_input, UserInput
 
-__version__ = '0.1.17'
+__version__ = '0.1.18'
 
 
 class GenBankRecord:
